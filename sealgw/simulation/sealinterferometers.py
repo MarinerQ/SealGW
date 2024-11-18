5e0d8b904c1760fc23aef5cad14877fff975c543
Qian Hu <54393783+MarinerQ@users.noreply.github.com>
2024-11-18 15:07:22 +0000

    remove gstlal

--- sealgw/simulation/sealinterferometers.py
+++ sealgw/simulation/sealinterferometers.py
@@ -28,6 +28,7 @@ from .generating_data import f_of_tau, tau_of_f, segmentize_tau
 from .antenna import GWAntennaOnCPU
 
 
+
 class SealInterferometer(Interferometer):
     """Class for the Interferometer for SealGW. ET response function aligned with LAL's."""
 
@@ -225,9 +226,11 @@ class SealInterferometer(Interferometer):
             # So that masked_index + masked_length = unmasked_index
             masked_length = len(self.frequency_array) - len(frequencies)
 
+
         else:
             mask = np.ones(len(frequencies), dtype=bool)
             masked_length = 0
+
 
         signal = {}
         if self.antenna_response_change:
@@ -255,12 +258,18 @@ class SealInterferometer(Interferometer):
                 self.antenna_response_change_timescale == 0
             ):  # use parallel calculation for all time(freq) points
                 # print('use parallel calculation for all time(freq) points')
+            if (
+                self.antenna_response_change_timescale == 0
+            ):  # use parallel calculation for all time(freq) points
+                print('use parallel calculation for all time(freq) points')
                 L = len(times)
                 ra_array = np.zeros(L) + parameters['ra']
-                dec_array = np.zeros(L) + parameters['dec']
-                psi_array = np.zeros(L) + parameters['psi']
+                dec_array = np.zeros(L)  + parameters['dec']
+                psi_array = np.zeros(L)  + parameters['psi']
                 fp, fc, dt = self.antenna_func.resp_and_dt(
+                    
                     ra_array, dec_array, times, psi_array
+                
                 )
                 antenna_response_array_dict = dict()
                 for mode in waveform_polarizations.keys():
@@ -270,10 +279,14 @@ class SealInterferometer(Interferometer):
                 antenna_response_array_dict['plus'][masked_length:] = fp
                 antenna_response_array_dict['cross'][masked_length:] = fc
                 time_shift = dt
-            else:  # use segment calculation. assume earth is fixed within each segment
-                # print('use segment calculation. assume earth is fixed within each segment')
+            else:   # use segment calculation. assume earth is fixed within each segment
+                # print(
+                    'use segment calculation. assume earth is fixed within each segment'
+                )
                 segment_starts = segmentize_tau(
+                    
                     tau, self.antenna_response_change_timescale
+                
                 )
 
                 antenna_response_array_dict = dict()
@@ -339,6 +352,7 @@ class SealInterferometer(Interferometer):
 
             dt = dt_geocent + time_shift
 
+        else:  #  not include earth rotation
         else:  #  not include earth rotation
             print('not including the earth rotation')
             for mode in waveform_polarizations.keys():
@@ -550,6 +564,7 @@ class SealTriangularInterferometer(InterferometerList):
             )
 
             # was "+" in bilby. Changing to - could reduces the difference with LAL but still has ~1e-4 error
+            xarm_azimuth -= 240
             xarm_azimuth -= 240
             yarm_azimuth -= 240
 
